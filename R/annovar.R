faefa788ada7e00c7642baf1b24874d00e878f11
Rahul Dhodapkar <rahuldhodapkar@vpn172022120247.its.yale.internal>
2019-07-21 13:44:48 -0400

    add annovar as melange input

---
+++ R/annovar.R
@@ -0,0 +1,155 @@
+# annovar.R
+#     Copyright (C) 2019  Rahul Dhodapkar
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Affero General Public License as
+#     published by the Free Software Foundation, either version 3 of the
+#     License, or (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the GNU Affero General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+#' Helper function to concatenate the unique identifiers for a variant
+#'
+#' @param df dataframe read from annovar output file
+#'
+#' @return A vector containing all unique variant identifiers present
+#'      in an annovar variant file.
+#'
+generate.varid.from.annovar.vff <- function(df) {
+    colname2ix <- hashmap(
+        c("CHR", "POS_START", "POS_END", "REF", "ALT"),
+        c(    3,           4,         5,     6,     7)
+    )
+
+    return(paste(
+            as.character(df[,colname2ix[["CHR"]]]),
+            as.character(df[,colname2ix[["POS_START"]]]),
+            as.character(df[,colname2ix[["POS_END"]]]),
+            as.character(df[,colname2ix[["REF"]]]),
+            as.character(df[,colname2ix[["ALT"]]]),
+            sep="."))
+}
+
+#' Helper function to group variants by gene.
+#'
+#' @param df dataframe read from annovar output file
+#'
+#' @return A hashmap from gene name -> variant counts
+#'
+group_by_gene <- function(df) {
+    GENE_COL <- 2
+    df$clean.gene.col <- gsub("\\(.*","", df[,GENE_COL])
+    freq.table <- as.data.frame(table(df$clean.gene.col))
+    return(hashmap(as.character(freq.table[,1]),freq.table[,2]))
+}
+
+#' Load data from annovar-annotated variant calls.
+#'
+#' \code{load.annovar} loads Annovar data for the provided cell to file mapping.
+#' By default, does not perfom any quality control on variant call files.
+#' \code{load.annovar} should be provided \code{*.variant_function} files
+#' for each cell. If additional exonic functional pruning is required,
+#' \code{load.annovar} will automatically extract the location of the corresponding
+#' \code{*.exonic_variant_function} file locally.
+#'
+#' @param cells character vector containing cell IDs
+#' @param annovar.filenames character vector containing annovar file paths.
+#' @param reduction reduction to use from annovar to Seurat assay.
+#'     Currently 'by_gene'
+#'
+#' @param germline.filename file path for annovar file containing germline 
+#'     variants. Used as a positive control for single-cell somatic 
+#'     variant analysis.
+#' @param spike.in.regex regex selection for 'CHR' vcf field inidcating
+#'     synthetic spike-in. Used as a negative control for single-cell
+#'     somatic variant analysis. Default '^ERCC-'
+#' @param somatic.only boolean, default TRUE, produce counts based only
+#'     on predicted somatic variants (subtact germline)
+#' 
+#' @return A matrix containing the reduced counts of each ENSG merged from
+#'     all cells supplied in a Seurat-importable format.
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' load.annovar(c('1', '2'), 
+#'     c('1.anno.variant_function', '2.anno.variant_function')
+#' )
+#' 
+#' }
+#'
+#' @importFrom hashmap hashmap
+#' @importFrom utils read.table
+#' @importFrom utils txtProgressBar
+#' @importFrom utils setTxtProgressBar
+#'
+#' @rdname load.annovar
+#' @export load.annovar
+#'
+load.annovar <- function(cells, annovar.filenames, 
+                            reduction="by_gene", somatic.only=TRUE,
+                            germline.filename=NULL, spike.in.regex="^ERCC-") {
+
+    if (! is.null(germline.filename) ) {
+        germline_df <- read.table(germline.filename, sep="\t")
+        germline_varstrings <- generate.varid.from.annovar.vff(germline_df)
+    } else {
+        germline_varstrings = c()
+    }
+    germ.map <- hashmap(
+        germline_varstrings, rep(1, length(germline_varstrings))
+    )
+
+    pb <- txtProgressBar(min = 0,
+        max = length(cells),
+        style = 3)
+
+    grouped_count_maps <- c()
+
+    for (i in 1:length(cells)) {
+        temp_df <- read.table(annovar.filenames[[i]], header = FALSE, sep = "\t")
+        temp_varstrings <- generate.varid.from.annovar.vff(temp_df)
+
+        germline_hits <- is.na(germ.map[[temp_varstrings]])
+        num_germline_hits <- sum(germline_hits, na.rm = TRUE)
+
+        if (somatic.only) {
+            temp_df <- temp_df[!is.na(germ.map[[temp_varstrings]]),]
+        }
+
+        temp_counts_map <- group_by_gene(temp_df)
+        grouped_count_maps <- c(grouped_count_maps, temp_counts_map)
+
+        setTxtProgressBar(pb, i)
+    }
+
+    nonzero_keys <- unique(
+        c(unlist(sapply(grouped_count_maps, function(a) a$keys())))
+    )
+
+    M <- matrix(0, nrow = length(nonzero_keys), ncol = length(cells))
+    rownames(M) <- nonzero_keys
+    colnames(M) <- cells
+
+    for (i in 1:length(cells)) {
+        colvals <- grouped_count_maps[[i]][[nonzero_keys]]
+        colvals[is.na(colvals)] <- 0
+        M[,i] = colvals
+    }
+
+    close(pb)
+
+    return(M)
+}
+
+
+
+
---
+++ tests/testthat/test_load_annovar.R
@@ -0,0 +1,36 @@
+# test_load_annovar.R
+#     Copyright (C) 2019  Rahul Dhodapkar
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Affero General Public License as
+#     published by the Free Software Foundation, either version 3 of the
+#     License, or (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the GNU Affero General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+context("LoadAnnovar")
+library(melange)
+
+dname = "../testdata/annovar"
+
+cells <- c("cell1", "cell2", "cell3")
+annovar_files <- paste(cells, "anno.variant_function", sep=".")
+annovar_files_fullpath <- paste(dname, annovar_files, sep="/")
+
+germline.filename <- paste(dname, 'germline.anno.variant_function', sep='/')
+
+annovar_matrix <- load.annovar(
+    cells=cells,
+    annovar.filenames=annovar_files_fullpath,
+    germline.filename=germline.filename
+)
+
+test_that("RSEM files to matrix", {
+  expect_is(annovar_matrix, "matrix")
+})
